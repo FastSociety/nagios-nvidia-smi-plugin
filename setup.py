29513bfb794d501bd7c65c000ae93b7b5b513f31
William Hutson <wilrnh@msn.com>
2014-01-16 19:17:11 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nagios-nvidia-smi-plugin',
+        version='0.2.0',
+        author='William Hutson',
+        author_email='wilrnh@gmail.com',
+        license="MIT",
+        keywords="nagios nvidia smi gpu plugin",
+        url='https://github.com/FastSociety/nagios-nvidia-smi-plugin',
+        description='Nagios plugin to check Nvidia GPU status using nvidia-smi',
+        long_description=open('README.txt').read(),
+        install_requires=["argparse","nagiosplugin"],
+        scripts=["check_nvidiasmi.py"]
+)
