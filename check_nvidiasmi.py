b7c5d3013af9aa9cba3b6a2e213d8f8463c883ef
Ron Moran <ron.moran@nielsen.com>
2018-03-06 15:23:19 +0200

    Some conventions and indentations refurbish

--- check_nvidiasmi.py
+++ check_nvidiasmi.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python
 
-import argparse, logging, nagiosplugin, subprocess, xml.etree.ElementTree
+import argparse
+import nagiosplugin
+import subprocess
+from xml.etree import ElementTree
+
 
 class Utilization(nagiosplugin.Resource):
 
     def __init__(self, args):
         self.args = args
-        nvidia_smi_proc = subprocess.Popen(["/usr/bin/nvidia-smi", "-q", "-x", "-i", args.device], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        nvidia_smi_proc = subprocess.Popen(["/usr/bin/nvidia-smi", "-q", "-x", "-i", args.device],
+                                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         nvidia_smi_proc_out, nvidia_smi_proc_err = nvidia_smi_proc.communicate()
         if nvidia_smi_proc.returncode > 0:
             raise Exception(nvidia_smi_proc_err)
         self.fb_memory = args.memory
-        self.nvidia_smi_xml_root = xml.etree.ElementTree.fromstring(nvidia_smi_proc_out)
+        self.nvidia_smi_xml_root = ElementTree.fromstring(nvidia_smi_proc_out)
 
     def probe(self):
         for gpu in self.nvidia_smi_xml_root.iter('gpu'):
             utilization = gpu.find('utilization')
-            gpuUtilization = float(utilization.find('gpu_util').text.strip(' %'))
-            yield nagiosplugin.Metric('gpuutil', gpuUtilization, '%')
+            gpu_utilization = float(utilization.find('gpu_util').text.strip(' %'))
+            yield nagiosplugin.Metric('gpuutil', gpu_utilization, '%')
             if self.fb_memory:
                 fb_mem = gpu.find('fb_memory_usage')
                 total_fb = float(fb_mem.find('total').text.strip(' MiB'))
                 used_fb = float(fb_mem.find('used').text.strip(' MiB'))
-                memUtilization = float("%.2f" % (used_fb / total_fb)) * 100
+                mem_utilization = float("%.2f" % (used_fb / total_fb)) * 100
             else:
-                memUtilization = float(utilization.find('memory_util').text.strip(' %'))
-            yield nagiosplugin.Metric('memutil', memUtilization, '%')
+                mem_utilization = float(utilization.find('memory_util').text.strip(' %'))
+            yield nagiosplugin.Metric('memutil', mem_utilization, '%')
 
             temperature = gpu.find('temperature')
-            gpuTemp = float(temperature.find('gpu_temp').text.strip(' C'))
-            yield nagiosplugin.Metric('gpuTemp', gpuTemp, '')
+            gpu_temp = float(temperature.find('gpu_temp').text.strip(' C'))
+            yield nagiosplugin.Metric('gpuTemp', gpu_temp, '')
+
 
 @nagiosplugin.guarded
 def main():
@@ -60,12 +66,13 @@ def main():
     args = argp.parse_args()
 
     check = nagiosplugin.Check(
-            Utilization(args),
-            nagiosplugin.ScalarContext('gpuutil', args.gpu_warning, args.gpu_critical),
-            nagiosplugin.ScalarContext('memutil', args.mem_warning, args.mem_critical),
-            nagiosplugin.ScalarContext('gpuTemp', args.gputemp_warning, args.gputemp_critical)
-            )
+        Utilization(args),
+        nagiosplugin.ScalarContext('gpuutil', args.gpu_warning, args.gpu_critical),
+        nagiosplugin.ScalarContext('memutil', args.mem_warning, args.mem_critical),
+        nagiosplugin.ScalarContext('gpuTemp', args.gputemp_warning, args.gputemp_critical)
+    )
     check.main(verbose=args.verbose)
+
 
 if __name__ == "__main__":
     main()
